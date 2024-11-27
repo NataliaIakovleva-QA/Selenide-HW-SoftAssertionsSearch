e8ca64cac7ee165c715fdab7992d52fccc698802
Natalia Iakovleva <natalia_yakovleva@yahoo.com>
2024-11-27 07:10:43 +0300

    @ExtendWith({SoftAssertsExtension.class}) search modified

--- src/test/java/github/SoftAssertionsSearchTest.java
+++ src/test/java/github/SoftAssertionsSearchTest.java
@@ -26,20 +26,16 @@ public class SoftAssertionsSearchTest {
     $("a[href=\"/selenide/selenide/wiki/SoftAssertions\"]").click();
     
     // проверить, что внутри есть пример кода для JUnit5
-    SelenideElement testCodePRE =
-    $$("h4.heading-element")
-            .findBy(text("3. Using JUnit5 extend test class:"))
-            .should(exist)
-            .closest(".markdown-heading")
-            .sibling(0)
-            .find("pre");
-
-    testCodePRE.shouldHave(text("@"));
-    testCodePRE.shouldHave(text("ExtendWith"));
-    testCodePRE.shouldHave(text("({"));
-    testCodePRE.shouldHave(text("SoftAssertsExtension"));
-    testCodePRE.shouldHave(text("."));
-    testCodePRE.shouldHave(text("class"));
-    testCodePRE.shouldHave(text("})"));
+    $("#wiki-body").shouldHave(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}"));
   }
 }
